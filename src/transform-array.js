5d1137dd6875dbdb73157aaaf1c25f0c5a356823
Barys Drahunou <barysdrahunou@gmail.com>
2020-09-04 13:01:28 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+Array.prototype.insert = function (index, item) {
+    this.splice(index, 0, item);
+};
+module.exports = function transform(array) {
+    let ancillaryArray = ["--discard-next", "--discard-prev", "--double-next", "--double-prev"]
+    if (!Array.isArray(array)) throw Error;
+    let intermediateArray = array.slice();
+    for (let i = 0; i < intermediateArray.length; i++) {
+        if (intermediateArray[i] === ancillaryArray[0]) {
+            if (i < intermediateArray.length - 1) {
+                intermediateArray[i + 1] = ancillaryArray[1];
+            }
+        }
+        if (intermediateArray[i] === ancillaryArray[1]) {
+            if (i > 0) {
+                intermediateArray[i - 1] = ancillaryArray[1];
+            }
+        }
+        if (intermediateArray[i] === ancillaryArray[2]) {
+            if (i < intermediateArray.length - 1 && ancillaryArray.indexOf(intermediateArray[i + 1]) === -1) {
+                intermediateArray[i] = intermediateArray[i + 1];
+            }
+        }
+        if (intermediateArray[i] === ancillaryArray[3]) {
+            if (i > 0 && ancillaryArray.indexOf(intermediateArray[i - 1]) === -1) {
+                intermediateArray[i] = intermediateArray[i - 1];
+            }
+        }
+    }
+    return intermediateArray.filter(x=>ancillaryArray.indexOf(x) === -1);
 };
