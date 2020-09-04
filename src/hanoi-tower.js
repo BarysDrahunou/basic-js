86d938e6a75bb023ec2c764c1f76e593e1e406f8
Barys Drahunou <barysdrahunou@gmail.com>
2020-09-04 13:29:23 +0300

    feat: make hanoi-tower more readable

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,12 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(discNumber,seconds) {
-  function calculate(discNumber){
-    if (discNumber===1) return 1;
-    return calculate(discNumber-1)*2+1;
-  }
-  let result={};
-  result.turns=calculate(discNumber);
-  result.seconds=Math.floor(result.turns*3600/seconds);
-  return result;
-};
+module.exports = function calculateHanoi(disksNumber, seconds) {
+    let turns = 2 ** disksNumber - 1;
+    return {
+        turns: turns,
+        seconds: Math.floor(3600 * turns / seconds),
+    }
+}
